74db1984853b7150daf5f335e90643f4eaa40ced
Maria Mendoza <maria.mendoza@slalom.com>
2021-04-22 13:15:04 -0400

    datastore setup

--- ml/infra/setup/datastore.py
+++ ml/infra/setup/datastore.py
@@ -5,8 +5,8 @@ import logging
 from azureml.core import Datastore, Workspace
 from project.util import CfgParser
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logger = logging.getLogger(__name__)
 
 def is_datastore_exists(workspace, name):
   try:
@@ -15,11 +15,20 @@ def is_datastore_exists(workspace, name):
   except HttpOperationError:
     return False
 
-def setup(config, secrets):
-  json_config = json.loads(config)
-  cfgparser = CfgParser()
-  cfg = cfgparser.parse(config, secrets)
+def register_datastore(workspace, datastore_config):
+  ds_name = ds_config.get("name")
 
+  if not is_datastore_exists(ws, ds_name):
+    Datastore.register_azure_blob_container(
+      workspace=ws,
+      datastore_name=ds_name,
+      account_name=ds_config.get("account_name"),
+      container_name=ds_config.get("container_name"),
+      account_key=ds_config.get("account_key"),
+      create_if_not_exists=ds_config.get("create_if_not_exists")
+    )
+
+def get_workspace(config):
   ws_config = cfg.get("workspace")
   ws_name = ws_config.get("name")
   ws_subscription_id = ws_config.get("subscription_id")
@@ -29,19 +38,18 @@ def setup(config, secrets):
                      subscription_id=ws_subscription_id,
                      resource_group=ws_resource_group)
 
+  return ws
+
+def setup(config, secrets):
+  json_config = json.loads(config)
+  cfgparser = CfgParser()
+  cfg = cfgparser.parse(config, secrets)
+
+  ws = get_workspace(config)
   datastores = ws_config.get("datastores")
 
   for ds_config in datastores:
-    ds_name = ds_config.get("name")
-    if not is_datastore_exists(ws, ds_name):
-      ds = Datastore.register_azure_blob_container(
-        workspace=ws,
-        datastore_name=ds_name,
-        account_name=ds_config.get("account_name"),
-        container_name=ds_config.get("container_name"),
-        account_key=ds_config.get("account_key"),
-        create_if_not_exists=ds_config.get("create_if_not_exists")
-      )
+    register_datastore(ws, ds_config)
 
 if __name__ == '__main__':
   
--- ml/project/util/cfgparser.py
+++ ml/project/util/cfgparser.py
@@ -1,27 +1,9 @@
 import json
 
+
 Class CfgParser:
   def __init__(self):
     self.secret_pattern = r"\$:(.+)"
-
-  def _recurse_keys(config):
-    """
-    Creates a nested list of keys extracted from config json/dict.
-    Called recursively.
-
-    Args:
-      config (dict): json/dict config
-    
-    Returns:
-      list: nested list of keys
-    """
-
-    keys = []
-    for k, v in config.items():
-
-
-
-
 
   def parse(self, config, secrets):
     """
@@ -41,41 +23,3 @@ Class CfgParser:
       new_config[k] = secrets.get(secret) or v
 
     return new_config
-
-Class WorkspaceCfgParser(CfgParser):
-
-  _CONFIG_TEMPLATE = { 
-    "workspace":
-      {
-        "name": "",
-        "subscription_id": "string",
-        "resource_group": "string",
-        "datastores": [
-          {
-            "name": "string",
-            "container_name": "string",
-            "account_name": "string",
-            "account_key": "string",
-            "create_if_not_exists": "True",
-            "subscription_id": "string",
-            "resource_group": "string"
-          },
-          {
-            "name": "string",
-            "container_name": "string",
-            "account_name": "string",
-            "account_key": "string",
-            "create_if_not_exists": "True",
-            "subscription_id": "string",
-            "resource_group": "string"
-          }
-        ]
-      }
-    }
-
-  def __init__(self):
-    super().__init__()
-  
-  def validate(self):
-
-
